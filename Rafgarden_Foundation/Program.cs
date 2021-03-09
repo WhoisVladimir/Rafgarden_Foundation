e4098a945b074cb3cd5b6503fcb3b63dec8a478a
Vladimir <bibubl@yandex.com>
2021-03-09 14:21:33 +0300

    Добавьте файлы проекта.

---
+++ Rafgarden_Foundation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rafgarden_Foundation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
